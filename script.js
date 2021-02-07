2774c7e7c6f09c2eda7ad68cbfd0e7b4abefd88d
john-hoey <john.t.hoey@gmail.com>
2021-02-07 18:32:26 -0500

    initial flip work completed and cleaned up the code.

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 "use strict";
-// let cards = [
-//   { name: `coin-ten`, img: `/assets/coin.png` },
-//   { name: `flower`, img: `/assets/flower.png` },
-//   { name: `mushroom`, img: `/assets/mushroom.png` },
-//   { name: `one-up-chest`, img: `/assets/one-up.png` },
-//   { name: `star`, img: `/assets/one-up.png` },
-//   { name: `twenty-coin`, img: `/assets/twenty-coin.png` },
-//   { name: `coin-ten`, img: `/assets/coin.png` },
-//   { name: `flower`, img: `/assets/flower.png` },
-//   { name: `mushroom`, img: `/assets/mushroom.png` },
-//   { name: `one-up-chest`, img: `/assets/one-up.png` },
-//   { name: `star`, img: `/assets/one-up.png` },
-//   { name: `twenty-coin`, img: `/assets/twenty-coin.png` },
-// ];
 let cards = [
-  { name: `coin-ten`, img: `/assets/coin.png` },
-  { name: `flower`, img: `/assets/flower.png` },
-  { name: `mushroom`, img: `/assets/mushroom1.png` },
-  { name: `one-up-chest`, img: `/assets/one-up-chest.png` },
-  { name: `star`, img: `/assets/star.png` },
-  { name: `twenty-coin`, img: `/assets/twenty-coin.png` },
-  { name: `coin-ten`, img: `/assets/coin.png` },
-  { name: `flower`, img: `/assets/flower.png` },
-  { name: `mushroom`, img: `/assets/mushroom1.png` },
-  { name: `one-up-chest`, img: `/assets/one-up-chest.png` },
-  { name: `star`, img: `/assets/star.png` },
-  { name: `twenty-coin`, img: `/assets/twenty-coin.png` },
+  { name: `coin-ten`, img: `assets/coin.png` },
+  { name: `flower`, img: `assets/flower.png` },
+  { name: `mushroom`, img: `assets/mushroom1.png` },
+  { name: `one-up-chest`, img: `assets/one-up-chest.png` },
+  { name: `star`, img: `assets/star.png` },
+  { name: `twenty-coin`, img: `assets/twenty-coin.png` },
+  { name: `coin-ten`, img: `assets/coin.png` },
+  { name: `flower`, img: `assets/flower.png` },
+  { name: `mushroom`, img: `assets/mushroom1.png` },
+  { name: `one-up-chest`, img: `assets/one-up-chest.png` },
+  { name: `star`, img: `assets/star.png` },
+  { name: `twenty-coin`, img: `assets/twenty-coin.png` },
 ];
 // EMPTY ARRAYS FOR MODIFICATIONS
-let cardData = [];
 let flippedCards = [];
-let matchCards = [];
+let matches = 0;
 
 // NAME THAT GRID
 const grid = document.querySelector(`.grid`);
@@ -42,92 +27,34 @@ const shuffleArray = (array) => {
     [array[i], array[j]] = [array[j], array[i]];
   }
 };
-// shuffleArray(cards);
 console.log(cards);
 
 //BUILD GRID OF CARDS
 const buildGrid = () => {
   shuffleArray(cards);
-  let i = 0;
-  // grid.removeChild(cardBox);
   grid.innerHTML = "";
-  cards.forEach((card) => {
-    let cardBox = document.createElement("img");
-    // let cardIMG = document.createElement("img");
-    cardBox.setAttribute(`data-id`, i);
-    cardBox.classList.add(`card`);
-    cardBox.addEventListener("click", cardFlip);
-    // cardBox.appendChild(cardIMG);
-    grid.appendChild(cardBox);
-    i++;
+  cards.forEach((item) => {
+    let card = document.createElement("div");
+    card.classList.add("flip-card");
+    let innerCard = document.createElement("div");
+    innerCard.classList.add("flip-card-inner");
+    let frontCard = document.createElement("div");
+    frontCard.classList.add("flip-card-front");
+    frontCard.style.backgroundImage = `url(assets/n-spade.png)`;
+    let backCard = document.createElement("div");
+    backCard.classList.add("flip-card-back");
+    backCard.style.backgroundImage = `url(${item.img})`;
+    // frontCard.append(frontImage);
+    innerCard.append(frontCard, backCard);
+    card.append(innerCard);
+    grid.append(card);
   });
 };
+buildGrid();
 // FLIP YOUR CARDS
 //bubble with delegation
 //if and only if I click on the card and itss face down and my flipped array is less than the array.length 2 then it will add card to array. it wont otherwise
-
-//
-const cardFlip = (e) => {
-  let cardId = e.target.getAttribute("data-id");
-  flippedCards.push(cards[cardId].name);
-  cardData.push(cardId);
-  // change src to css
-  e.target.setAttribute("src", cards[cardId].img);
-  // grid.style.pointerEvents = "none";
-  if (flippedCards.length === 2) {
-    // setTimeout(matchLogic, 500);
-  }
-};
-// console.log(cardFlip);
-console.log(flippedCards);
-buildGrid();
-
-// const cardFlip = (e) => {
-//   let cardId = e.target.getAttribute(`data-id`);
-//   if (e.target.className === "card" && flippedCards.length < 2) {
-//     flippedCards.push(e.target.cards[cardId].img);
-//   }
-//   //check length of flipped cards
-//   if (flippedCards.length === 1) {
-//     //e.target stop flip
-//     //if/else (match or doesn't)
-//     //matching function
-//     //      if matching push to match array
-//     if (
-//       flippedCards[0].img === flippedCards[1].img &&
-//       flippedCards[0].cardId !== flippedCards[1].cardId
-//     ) {
-//       matchCards.push(flippedCards);
-//       flippedCards = [];
-//       matchCards.classList.add("black");
-//     } else {
-//       setTimeout(() => {});
-//       flip;
-//     }
-//     //unmatching function
-//     //clear flipped cards array
-//     flippedCards = [];
-//   }
-// };
-// buildGrid();
-// cardBox.addEventListener("click", cardFlip);
-
-//-------------------------------------------------------------------------------------------------------------------
-//SOME OTHER MATCHING LOGIC
-// let matchLogic1 = () => {
-//   if (
-//     matchCards[0].class === matchCards[1].class &&
-//     matchCards[0].id !== matchCards[1].id
-//   ) {
-//     matchCards[0].style.display = "black";
-//     matchCards[1].style.display = "black";
-//   } else {
-//     matchCards[0].style.display = "card";
-//     matchCards[1].style.display = "card";
-//   }
-// };
-//-------------------------------------------------------------------------------------------------------------------
-
+grid.addEventListener("click", () => {});
 //TIMER FUNCTION
 let timer = document.querySelector(".timer");
 
@@ -155,6 +82,8 @@ const timerStart = () => {
 // // RESET TIMER
 let resetButton = document.querySelector(".reset-button");
 resetButton.addEventListener(`click`, () => {
+  secondsLabel.innerHTML = 0;
+  minutesLabel.innerHTML = 0;
   totalSeconds = 0;
 
   // let minutesLabel = document.getElementById("minutes");
